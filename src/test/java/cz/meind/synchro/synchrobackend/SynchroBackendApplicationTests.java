7ffcae61cae7f8c513fbd008835ac34e0e9dce3a
Meind <105383135+WMeindW@users.noreply.github.com>
2024-10-04 19:56:26 +0200

    Initial commit

---
+++ src/test/java/cz/meind/synchro/synchrobackend/SynchroBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.meind.synchro.synchrobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynchroBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
