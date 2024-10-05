5cc4eeec1ff60c7e41e796850212067240fa5c2d
Meind <105383135+WMeindW@users.noreply.github.com>
2024-10-05 18:27:43 +0200

    jwt setup

---
+++ src/main/java/cz/meind/synchro/synchrobackend/database/entities/UserEntity.java
@@ -0,0 +1,73 @@
+package cz.meind.synchro.synchrobackend.database.entities;
+
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class UserEntity implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "jwt_token")
+    private String jwtToken;
+
+    // Constructors
+    public UserEntity() {
+    }
+
+    public UserEntity(String username, String password, String email, String role) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public String getJwtToken() {
+        return jwtToken;
+    }
+
+    public void setJwtToken(String jwtToken) {
+        this.jwtToken = jwtToken;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
---
+++ src/main/java/cz/meind/synchro/synchrobackend/service/AuthenticationService.java
@@ -0,0 +1,39 @@
+package cz.meind.synchro.synchrobackend.service;
+
+import cz.meind.synchro.synchrobackend.database.entities.UserEntity;
+import cz.meind.synchro.synchrobackend.database.repositories.UserRepository;
+import cz.meind.synchro.synchrobackend.dto.LoginUserDto;
+import cz.meind.synchro.synchrobackend.dto.RegisterUserDto;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService {
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final AuthenticationManager authenticationManager;
+
+    public AuthenticationService(UserRepository userRepository, AuthenticationManager authenticationManager, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public UserEntity signup(RegisterUserDto input) {
+        UserEntity user = new UserEntity();
+        user.setUsername(input.getUsername());
+        user.setPassword(passwordEncoder.encode(input.getPassword()));
+
+        return userRepository.save(user);
+    }
+
+    public UserEntity authenticate(LoginUserDto input) {
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(input.getUsername(), input.getPassword()));
+
+        return userRepository.findByUsername(input.getUsername()).orElseThrow();
+    }
+}
