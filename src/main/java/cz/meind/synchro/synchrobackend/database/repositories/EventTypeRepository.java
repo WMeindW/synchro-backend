f8138e9d94ccd458ba9e41ab547e34816ffed4bb
WMeindW <daniellinda2005@gmail.com>
2024-10-16 19:29:29 +0200

    Adding events Entities

---
+++ src/main/java/cz/meind/synchro/synchrobackend/database/repositories/EventTypeRepository.java
@@ -0,0 +1,10 @@
+package cz.meind.synchro.synchrobackend.database.repositories;
+
+import cz.meind.synchro.synchrobackend.database.entities.EventTypeEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface EventTypeRepository extends JpaRepository<EventTypeEntity, Long> {
+    Optional<EventTypeEntity> findEventTypeEntitiesByName(String name);
+}
--- src/main/java/cz/meind/synchro/synchrobackend/service/events/EventTypesService.java
+++ src/main/java/cz/meind/synchro/synchrobackend/service/events/EventTypesService.java
@@ -1,10 +1,18 @@
 package cz.meind.synchro.synchrobackend.service.events;
 
+import cz.meind.synchro.synchrobackend.database.repositories.EventTypeRepository;
 import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EventTypesService {
+
+    private final EventTypeRepository eventTypeRepository;
+
+    public EventTypesService(EventTypeRepository eventTypeRepository) {
+        this.eventTypeRepository = eventTypeRepository;
+    }
+
     @PostConstruct
     private void initializeTypes() {
         System.out.println("Initilaized");
