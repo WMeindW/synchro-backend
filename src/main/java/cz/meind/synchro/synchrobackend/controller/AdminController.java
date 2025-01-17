aaa9914de70d1531141be195e4adc88a87537605
Meind <105383135+WMeindW@users.noreply.github.com>
2025-01-17 09:48:28 +0100

    update

--- src/main/java/cz/meind/synchro/synchrobackend/controller/AdminController.java
+++ src/main/java/cz/meind/synchro/synchrobackend/controller/AdminController.java
@@ -109,9 +109,8 @@ public class AdminController extends Controller {
     @CrossOrigin
     @GetMapping(value = "/query-summary", produces = "application/json")
     public ResponseEntity<?> querySummary(@RequestParam LocalDate month, HttpServletRequest request) {
-        /*if (!super.handleApiSecureRequest(request, config.getAdminRole()))
+        if (!super.handleApiSecureRequest(request, config.getAdminRole()))
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-         */
         return ResponseEntity.ok(informationService.querySummary(month));
     }
 }
