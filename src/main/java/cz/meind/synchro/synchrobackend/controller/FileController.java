67734dee011a9f6e56844623eb35032ec0f0d6cb
Meind <105383135+WMeindW@users.noreply.github.com>
2025-03-16 17:19:05 +0100

    update

---
+++ src/main/java/cz/meind/synchro/synchrobackend/controller/FileController.java
@@ -0,0 +1,40 @@
+package cz.meind.synchro.synchrobackend.controller;
+
+import cz.meind.synchro.synchrobackend.config.SynchroConfig;
+import cz.meind.synchro.synchrobackend.controller.main.Controller;
+import cz.meind.synchro.synchrobackend.service.user.FileService;
+import cz.meind.synchro.synchrobackend.service.user.auth.SecurityService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/files")
+public class FileController extends Controller {
+    private final SynchroConfig synchroConfig;
+    private final FileService fileService;
+
+    /**
+     * Constructs a new Controller with the specified SecurityService.
+     *
+     * @param securityService The security service responsible for user authentication and authorization.
+     */
+    public FileController(SecurityService securityService, SynchroConfig synchroConfig, FileService fileService) {
+        super(securityService);
+        this.synchroConfig = synchroConfig;
+        this.fileService = fileService;
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<?> upload(@RequestParam("file") MultipartFile file, @RequestParam("username") String username, HttpServletRequest request) {
+        if (!super.handleApiSecureRequest(request, synchroConfig.getCombinedRole()))
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        if (!fileService.uploadFile(file, username, request)) return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+}
---
+++ src/main/java/cz/meind/synchro/synchrobackend/service/user/FileService.java
@@ -0,0 +1,12 @@
+package cz.meind.synchro.synchrobackend.service.user;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileService {
+    public boolean uploadFile(MultipartFile file, String username, HttpServletRequest request){
+
+    }
+}
